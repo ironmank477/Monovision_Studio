2f638bc2e9b7fc32adc8269d68dbeb3a821766cc
Oleg <test@gmail.com>
2025-09-15 13:44:26 +0300

    initial commit

---
+++ src/js/style.js
@@ -0,0 +1,17 @@
+let sumArr = ["aple", 1, 45];
+
+// sumArr.forEach(function (item, index, array) {
+// 	console.log(`${item} Позиция ${index}, в массиве ${array}`);
+// })
+
+sumArr.forEach(function (item, index, arr) {
+	console.log(`${item} position ${index} in massive ${arr}`);
+});
+
+
+sumArr.forEach(function (item, index, arr) {
+
+})
+
+let b = new Array(10);
+console.log(b);
